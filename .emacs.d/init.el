d15767b7fb19ff5f92adc06d78d2d4cc46e24229
OKU1987 <oku.87.s62.a4@gmail.com>
2014-09-28 20:06:47 +0900

    Change key bindings for point-undo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -102,8 +102,8 @@
 
 ;; Point undo
 (when (require 'point-undo nil t)
-  (define-key global-map (kbd "M-[") 'point-undo)
-  (define-key global-map (kbd "M-]") 'point-redo)
+  (define-key global-map [f5] 'point-undo)
+  (define-key global-map [f6] 'point-redo)
 )
 
 ;; Highlight the current line on GDB source buffer
