3d8e5a821fd1a2a0c10d458603bbf725e4a4e11d
OKU1987 <oku.87.s62.a4@gmail.com>
2015-04-18 00:42:15 +0900

    Add lean-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,6 +162,32 @@
 ;; ssreflect
 (load-file "~/Coq/src/ssreflect-1.5/pg-ssr.el")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; lean
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Install required/optional packages for lean-mode
+(defvar lean-mode-required-packages
+  '(company dash dash-functional flycheck f
+	    fill-column-indicator s lua-mode mmm-mode))
+(let ((need-to-refresh t))
+  (dolist (p lean-mode-required-packages)
+    (when (not (package-installed-p p))
+      (when need-to-refresh
+	(package-refresh-contents)
+	(setq need-to-refresh nil))
+      (package-install p))))
+
+;; Set up lean-root path
+(setq lean-rootdir "/usr/local")
+(setq-local lean-emacs-path "/usr/local/share/emacs/site-lisp/lean")
+(add-to-list 'load-path (expand-file-name lean-emacs-path))
+(require 'lean-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Other settings
 (define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
 (define-key global-map (kbd "C-t") 'other-window)
